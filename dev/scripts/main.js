548da3d64cc9120d9e9b0b97a4659a7da1680b83
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2017-05-22 15:55:35 -0400

    finished and working

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -59,7 +59,7 @@ jQuery(function ($) {
                 } else {
                     trackNumber = '' + trackNumber;
                 }
-                $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
+                $('#plList').append('<li data-toggle="#numb' + trackNumber + '"><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
             }),
             trackCount = tracks.length,
             npAction = $('#npAction'),
@@ -129,9 +129,67 @@ jQuery(function ($) {
         loadTrack(index);
     }
 });
-$(document).ready(function(event){
-  if ( $("li:nth-child(4)").hasClass('plSel') ) {
-    $('.fullscreen-bg').removeClass('four' );
-  }
-});
+// curved svg 
+// $(document).ready(function(){
+//   var $oneAppend = $('<video loop muted autoplay poster="assets/one.png" class="fullscreen-bg__video"><source src="assets/one.mp4" type="video/mp4"><source src="assets/one.ogv" type="video/ogg"></video>');
+//   var $twoAppend = $('<video loop muted autoplay poster="assets/two.png" class="fullscreen-bg__video"><source src="assets/two.mp4" type="video/mp4"><source src="assets/two.ogv" type="video/ogg"></video>');
+//   var $threeAppend = $('<video loop muted autoplay poster="assets/three.png" class="fullscreen-bg__video"><source src="assets/three.mp4" type="video/mp4"><source src="assets/three.ogv" type="video/ogg"></video>');
+  
+//   if ($('#numb01').hasClass('plSel')) {
+//     // $('fullscreen-bg').removeClass('one');
+//     // $('fullscreen-bg').addClass('two', 'three', 'four' ,'five','six');
+//     $oneAppend.appendTo('#fuckoff');
+//     $twoAppend.remove();
+//     $threeAppend.remove();
+//    } 
 
+//   if ($('#numb02').hasClass('plSel')) {
+//     // $('fullscreen-bg').removeClass('one');
+//     // $('fullscreen-bg').addClass('two', 'three', 'four' ,'five','six');
+//     $oneAppend.remove();
+//     $threeAppend.remove();
+//     $twoAppend.appendTo('#fuckoff');
+//    } 
+//   if ($('#numb03').hasClass('plSel')) {
+//     // $('fullscreen-bg').removeClass('one');
+//     // $('fullscreen-bg').addClass('two', 'three', 'four' ,'five','six');
+//     $threeAppend.appendTo('#fuckoff');
+//     $oneAppend.remove();
+//     $twhoAppend.remove();
+//    } 
+   
+// });
+
+
+
+function curvedText(time) {
+  var tl   = new TimelineMax({ repeat: -1 });
+  var text = document.querySelector('svg textpath'),
+      path = document.querySelector('svg defs path');
+
+  var from = {
+    transformOrigin: 'center center',
+    rotation: 0
+  };
+
+  var to = {
+    rotation: 360,
+    ease: Linear.easeInOut
+  };
+
+  tl.fromTo([text, path], time, from, to);
+
+  return tl;
+}
+
+curvedText(20);
+
+//toggle of things
+$(document).on('click', '[data-toggle]', function () {
+  var targetId = $(this).data('toggle')
+  targetId = $(targetId)
+  var index = targetId.index()
+
+  targetId.show().siblings().hide()
+  $('.fullscreen-bg:eq('+ index +')').show().siblings().hide()
+})
